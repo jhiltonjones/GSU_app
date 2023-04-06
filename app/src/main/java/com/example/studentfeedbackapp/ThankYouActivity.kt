751371fbb59258650c9b989b57bbc9769c3ae958
jhiltonjones <124688978+jhiltonjones@users.noreply.github.com>
2023-04-06 18:15:01 -0400

    Add Home menu

--- app/src/main/java/com/example/studentfeedbackapp/ThankYouActivity.kt
+++ app/src/main/java/com/example/studentfeedbackapp/ThankYouActivity.kt
@@ -1,6 +1,8 @@
 package com.example.studentfeedbackapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -8,5 +10,12 @@ class ThankYouActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.submit_feedback_confirmation)
+
+        val writeReviewButton = findViewById<Button>(R.id.btn_home_menu)
+
+        writeReviewButton.setOnClickListener {
+            val intent = Intent(this, ClassList::class.java)
+            startActivity(intent)
+        }
     }
 }
