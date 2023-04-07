a6d79e732b7b9257f94d659b345b3d95b02c9292
ThienNguyen1001 <123020604+ThienNguyen1001@users.noreply.github.com>
2023-04-06 21:47:23 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/studentfeedbackapp/ReviewActivity.kt
@@ -0,0 +1,23 @@
+package com.example.studentfeedbackapp
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
+class ReviewActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review)
+
+        val button = findViewById<Button>(R.id.submit)
+
+        button.setOnClickListener {
+            // Code to run when the button is clicked
+            val intent = Intent(this, FeedbackActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
