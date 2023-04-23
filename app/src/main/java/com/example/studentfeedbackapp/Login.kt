692fdba675f70b7e11961d33bfe2bcdc7e881478
jhiltonjones <124688978+jhiltonjones@users.noreply.github.com>
2023-04-22 22:30:21 -0400

    Updated with log in

--- app/src/main/java/com/example/studentfeedbackapp/Login.kt
+++ app/src/main/java/com/example/studentfeedbackapp/Login.kt
@@ -1,20 +1,56 @@
 package com.example.studentfeedbackapp
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
         val submitButton = findViewById<Button>(R.id.btn_login)
+        val emailEditText = findViewById<EditText>(R.id.edit_email)
+        val passwordEditText = findViewById<EditText>(R.id.edit_password)
+        val registerButton = findViewById<Button>(R.id.text_register)
+
+        // Initialize Firebase Authentication instance
+        auth = FirebaseAuth.getInstance()
 
         // Set up click listener for the submit button
         submitButton.setOnClickListener {
-            // Launch the ClassList activity
-            val intent = Intent(this, ClassList::class.java)
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            // Authenticate user with email and password
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = auth.currentUser
+                        Toast.makeText(this, "Authentication success.",
+                            Toast.LENGTH_SHORT).show()
+                        // Launch the ClassList activity
+                        val intent = Intent(this, ClassList::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        // Set up click listener for the register button
+        registerButton.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/example/studentfeedbackapp/Register.kt
@@ -0,0 +1,50 @@
+package com.example.studentfeedbackapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class Register : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+
+        // Initialize Firebase Authentication instance
+        auth = FirebaseAuth.getInstance()
+
+        val registerButton = findViewById<Button>(R.id.register_button)
+        val emailEditText = findViewById<EditText>(R.id.register_email)
+        val passwordEditText = findViewById<EditText>(R.id.register_password)
+
+        registerButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+
+            // Call the Firebase Authentication API to create a new user with the specified email and password
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // User registration successful, navigate to the ClassList activity
+                        val intent = Intent(this, ClassList::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // User registration failed, display an error message
+                        Toast.makeText(
+                            this,
+                            "Registration failed: " + task.exception?.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+}
