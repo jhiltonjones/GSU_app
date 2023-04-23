8577448c53094eec8cf7e8bb8ab5f260166e59a9
Manggoz <99453089+Manggoz@users.noreply.github.com>
2023-04-23 19:41:31 -0400

    profile
    profile now prints all the information but it is stacked on top of each other need to fix

--- app/src/main/java/com/example/studentfeedbackapp/Profile.kt
+++ app/src/main/java/com/example/studentfeedbackapp/Profile.kt
@@ -1,18 +1,31 @@
 package com.example.studentfeedbackapp
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 
 class Profile : AppCompatActivity() {
 
+
+    lateinit var User_name: TextView
+    lateinit var User_Age: TextView
+    lateinit var User_major: TextView
+    lateinit var User_ID: TextView
+    lateinit var User_grad: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile)
+
+        User_name = findViewById(R.id.user_name)
+        User_Age = findViewById(R.id.age_text)
+        User_major = findViewById(R.id.major)
+        User_ID = findViewById(R.id.Student_id)
+        User_grad = findViewById(R.id.graduating)
 
         // Get the current user email from the intent
         val currentUser = intent.getStringExtra("useremail") ?: ""
@@ -35,6 +48,11 @@ class Profile : AppCompatActivity() {
                         val id = document.getString("StudentID")
                         val email = document.getString("Email")
                         // TODO: Do something with the retrieved user info
+                        User_name.setText("Welcome "+name+"!")
+                        User_Age.setText("Age:"+age)
+                        User_major.setText("Major:")
+                        User_ID.setText("Student ID: "+id)
+                        User_grad.setText("Graduating year: ")
                         println("this runs")
                         println(age)
                         println(name)
